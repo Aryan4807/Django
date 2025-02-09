b2da02dcea62da0cc67b90c09e80d322044757c3
Aryan4807 <65225779+Aryan4807@users.noreply.github.com>
2025-02-09 13:09:39 +0530

    changes for django project

--- hi/hi/settings.py
+++ hi/hi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'hi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# added manually
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # "/var/www/static/",
+]
--- hi/home/views.py
+++ hi/home/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome to my routing bro")
+    context={
+        "variablq":"dsasda"
+    }
+    return render(request,"index.html",context)
+    # return HttpResponse("welcome to my routing bro")
 
 def contact(request):
     return HttpResponse("my contact are huge")
 def about(request):
-    return HttpResponse("i will be kickass ")
+    return HttpResponse("About us ")
+def services(request):
+    return HttpResponse("services Contact us ")
